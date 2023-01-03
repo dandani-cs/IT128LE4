6a123db2e586d472be0c52f65c0d8c460f25167c
Dani <dani.cstech@gmail.com>
2023-01-03 19:15:31 +0800

    logout functionality successful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SAMONTELE4';
+
+  constructor(private tokenStorageService: TokenStorageService) {
+    
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
+
+
