13fc17bf6c95888f1b5a43692084ba3c3e8193e0
Dani <dani.cstech@gmail.com>
2023-01-18 20:46:46 +0800

    adding post detail functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { ListPostsComponent } from './components/list-posts/list-posts.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     RegisterPageComponent,
-    ListPostsComponent
+    ListPostsComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
